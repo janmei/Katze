9766a03739768999086de40e28e696170b480fca
Jan <jan.meininghaus@web.de>
2018-02-27 14:50:08 +0100

    basic socket connection

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/private/index.html');
+});
+
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, function(){
