80ce7af7606628e38880a462bdb3ae15250f48aa
Jan Meininghaus <jan.meininghaus@stud.h-da.de>
2018-02-28 01:13:56 +0100

    fixed file routing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var serveStatic = require('serve-static');
+
+app.use(express.static('static'))
 
 app.get('/admin', function(req, res){
   res.sendFile(__dirname + '/private/index.html');
@@ -14,9 +18,9 @@ io.on('connection', function (socket) {
   });
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-});
+
+  app.use(express.static('public'));
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
